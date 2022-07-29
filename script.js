3fab563dc7789c8f7a3da73cf5244a88ace22368
awaisali <awais.ali@tintash.com>
2022-07-30 00:46:48 +0500

    feat: complete and remove task

--- script.js
+++ script.js
@@ -1,55 +1,75 @@
 "use strict";
-// let taskInput = document.querySelector("input").textContent;
-// taskInput = "awais";
-// console.log(taskInput);
-// console.log("Awais");
 
-const containerTasks = document.querySelector("#task__list");
-const taskButton = document.getElementById("add");
-
-// Class of TODO LIST
 let tasksArray = [];
 
+const containerTasks = document.querySelector("#task__list");
+containerTasks.classList.add("displayRemove");
+const taskButton = document.getElementById("add");
+const completeButton = document.querySelector(".complete");
+
+function completeTask(event, i) {
+  tasksArray[i].isComplete = tasksArray[i].isComplete ? false : true;
+  renderHTML();
+}
+
+function removeTask(event, i) {
+  tasksArray.splice(i, 1);
+  containerTasks.innerHTML = "";
+  renderHTML();
+}
+
+// Class of TODO LIST
+
 class Todo {
-  constructor(title, date, time) {
+  constructor(title, date, time, str, isComplete) {
     this.title = title;
     this.date = date;
     this.time = time;
+    this.str = str; // AM/PM
+    this.isComplete = isComplete;
   }
 }
 
-function renderHTML(str) {
+function renderHTML() {
+  containerTasks.innerHTML = "";
+  let str = "";
   tasksArray.map((el, i) => {
-    console.log(el.title);
-    const html = `<div class="task">
+    console.log(el.title, el.isComplete);
+    str = el.isComplete ? "completeTask" : "";
+    const html = `<div class="task ${str}">
           <div class="task__content">
             <div class="title">
               <i class="fa-solid fa-list-check"></i>
               <h2>Task ${i + 1}:</h2>
               <p>${el.title}</p>
             </div>
-            <div class="date__time"><p>${el.date} @ ${el.time} ${str}</p></div>
+            <div class="date__time">
+                <p>${el.date} @ ${el.time} ${el.str}</p>
+            </div>
             <div class="options">
               <div class="btns">
                 <button class="edit">
                   <i class="fa-solid fa-italic"></i>
                 </button>
-                <button class="complete">
-                  <i class="fa-solid fa-circle-check"></i>
-                </button>
-                <button class="remove">
+                <button class="complete" onclick="completeTask(event,${i})">
+                    <i class="fa-solid fa-circle-check"></i>
+                  </button>
+                <button class="remove" onclick="removeTask(event,${i})">
                   <i class="fa-solid fa-trash"></i>
                 </button>
               </div>
             </div>
           </div>
         </div>`;
-    containerTasks.insertAdjacentHTML("afterbegin", html);
+    containerTasks.insertAdjacentHTML("beforeend", html);
   });
 }
 
+console.log(completeButton.addEventListener("click", () => {}));
+
 taskButton.addEventListener("click", function () {
   containerTasks.innerHTML = "";
+  containerTasks.classList.remove("displayRemove");
 
   const taskInput = document.querySelector("input").value;
   document.querySelector("input").value = "";
@@ -69,8 +89,10 @@ taskButton.addEventListener("click", function () {
   const date = `${dateDay}/${dateMonth}/${dateYear}`;
   const time = `${timeHour}:${timeMin}`;
 
-  tasksArray.push(new Todo(taskInput, date, time));
+  tasksArray.push(new Todo(taskInput, date, time, str, false));
 
   //render
-  renderHTML(str);
+  renderHTML();
 });
+
+console.log(tasksArray);
